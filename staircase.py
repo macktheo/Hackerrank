5980ebcbebcc3b5c7ed45a5518dcef3c2bd58a8a
macktheo <94210515+macktheo@users.noreply.github.com>
2022-03-04 00:07:08 +0700

    Create staircase.py

---
+++ staircase.py
@@ -0,0 +1,15 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+def staircase(n):
+    for i in range (1, n+1):
+        s ="#" * i
+        print(s.rjust(n))
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    staircase(n)
