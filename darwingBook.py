d532205e77c5b1631b8986fb9d9a4880d46eab81
macktheo <94210515+macktheo@users.noreply.github.com>
2022-03-12 10:08:58 +0700

    Create darwingBook.py

---
+++ darwingBook.py
@@ -0,0 +1,33 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'pageCount' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER p
+#
+
+def pageCount(n, p):
+     depan = p // 2
+     belakang = (n-p)//2 + ( 1 if p%2!=0 and n-p == 1 else 0 )
+     return belakang if belakang < depan else depan
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    p = int(input().strip())
+
+    result = pageCount(n, p)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
